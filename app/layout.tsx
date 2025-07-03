2a1b1f16f44c218e5401b95321ed9e6f5354cb87
Shubhkarman-Singh <shubhkarmansingh29ankurschool@gmail.com>
2025-07-03 21:56:42 +0530

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "AskClyra",
+  description: "Your personal AI Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "AskClyra",
     statusBarStyle: "default",
   },
 };
